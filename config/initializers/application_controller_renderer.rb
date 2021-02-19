aa87dfc287969a5f39125828f2541ecd9f271f47
EisukeTochitani <kop.michael1958@gmail.com>
2021-02-19 13:41:07 +0000

    [finish]4-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e8e4e1a0127a5ba0cda3c0de5ee9eca97c130680f979265a9344ca0c8ffd88d4e2574bafa60cbaf97838bce2ee319819363994221e759cf853e3110b13ba40e2'
